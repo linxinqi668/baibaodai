28aa0e5101a37595b69047db0126614e16b3ade4
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 23:56:00 +0800

    >  compile NEMU 3019244317 lxq Linux lxq-KLV-WX9 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:56:00 up  1:41,  1 user,  load average: 0.86, 0.57, 0.54 da92a3de8c2887672b21a775c9e38edc6d53e

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,15 +15,19 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
+
+	uint32_t ecx, edx, ebx, esp, ebp, esi, edi;
+
 	union {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
 	} gpr[8];
 
-	/* Do NOT change the order of the GPRs' definitions. */
+	#define eax gpr[0]._32
 
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+
+	/* Do NOT change the order of the GPRs' definitions. */
 
 	swaddr_t eip;
 
