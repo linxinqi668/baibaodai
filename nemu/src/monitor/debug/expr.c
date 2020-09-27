86a29a326a465032192fdd6148318b185e89c506
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 16:55:50 +0800

    >  compile NEMU 3019244317 lxq Linux lxq-KLV-WX9 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:55:50 up  9:43,  1 user,  load average: 0.87, 0.57, 0.43 fe850219e2de5eadc41fa7ec615aa7ee19c9c74

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -109,8 +109,6 @@ static bool make_token(char *e) {
 				// 记录token的类型
 				tokens[ nr_token ].type = rules[i].token_type;
 
-				Assert(substr_len <= 31, "oh ho buf overflow!");
-
 				// 开辟一块新的空间来保存该子串, 留一个字符作为结束符
 				char * new_space = (char *)malloc(substr_len + 1);
 				strncpy(new_space, substr_start, substr_len);
@@ -131,6 +129,7 @@ static bool make_token(char *e) {
 				Assert(nr_token <= 32, "the tokens array over flow!");
 				
 
+				// 一些特殊的token必须经过某些处理, 现在还用不到
 				switch(rules[i].token_type) {
 					// default: panic("please implement me");
 				}
@@ -148,10 +147,243 @@ static bool make_token(char *e) {
 	return true; 
 }
 
-// stack
+/* char_stack *************************************************************/
+typedef struct {
+	char * element;
+	int top; // 栈顶指针 未放入
+	int max_size; // 栈最大元素个数
+} stack, *stack_p; // stack_p是指向栈结构体的指针
+
+stack_p Creat_Stack(int max_size){
+	// 开辟空间
+	stack_p new_stack = (stack_p) malloc(sizeof(stack));
+	new_stack->element = (char *) malloc(sizeof(max_size+2));
+	memset(new_stack->element, 0, sizeof(max_size+2)); // 清空字符数组
+
+	// 初始化
+	new_stack->max_size = max_size;
+	new_stack->top = 0;
+
+	return new_stack;
+}
+
+void Destory_Stack(stack_p x){
+	free(x->element);
+	free(x);
+}
+
+bool is_empty(stack_p x){
+	if (x->top == 0)
+		return true;
+	return false;
+}
+
+void push(stack_p x, char c){
+	Assert(x->top < x->max_size, "栈已满.");
+	x->element[ x->top++ ] = c;
+}
+
+void pop(stack_p x){
+	Assert(!is_empty(x), "栈空, 但是进行了pop操作.");
+	x->top--;
+}
+
+char top(stack_p x){
+	Assert(!is_empty(x), "栈空, 无top元素.");
+	return x->element[x->top-1];
+}
+/*********************************************************************/
+
+/* 判断括号是否匹配 *****************************************************/
+bool is_match(char * str){
+    int len = strlen(str);
+
+    // 开辟一个栈
+    stack_p s = Creat_Stack(len);
+
+    // 进行匹配
+    for (int i = 0; i < len; i++)
+        switch (str[i]){
+            case '(':{
+                push(s, str[i]); break;
+            }
+            case ')':{
+                if (is_empty(s))
+                    return false;
+                else if (top(s) == '(')
+                    pop(s);
+                else
+                    push(s, ')');
+				break;
+            }
+			default: break;
+        }
+    
+    // 判断结果
+    if (is_empty(s)){
+        Destory_Stack(s);
+        return true;
+    }
+    else {
+        Destory_Stack(s);
+        return false;
+    }
+}
+/*****************************************************************/
+
+/* sub_str *******************************************************/
+char * sub_str(char * str, int p, int q){
+	// 返回p至q的子串
+	Assert(p <= q, "p,q传参错误");
+
+	char * res = (char *)malloc(q - p + 1 + 1); // 留一个结束符
+
+	strncpy(res, str + p, q - p + 1);
+	res[q-p+1] = '\0'; // 写入结束符
+
+	return res;
+}
+/*****************************************************************/
+
+/* 判断是否是配对的表达式(通过样例) ***********************************/
+bool check_parentheses(char * expression, int p, int q){
+	// 获取p至q子串
+	char * temp = sub_str(expression, p, q);
+	int len = strlen(temp);
+
+	// 首先, 表达式的开头与结尾必须是一对括号
+	if (temp[0] != '(' || temp[len-1] != ')') {
+		free(temp);
+		return false;
+	}
+
+	// 其次, 表达式必须符合括号匹配
+	if (!is_match(temp)){
+		free(temp);
+		return false;
+	}
+	
+	// 最后, 确保两边的括号是相互匹配的。
+	// 要完成这一步, 只要保证剩余括号相互匹配即可
+	char * sub_temp = sub_str(temp, 1, len-2);
+
+	Assert(strlen(sub_temp) == len-2, "子串错误！");
+
+	if (!is_match(sub_temp)){
+		free(temp); free(sub_temp);
+		return false;
+	}
+
+	free(temp); free(sub_temp);
+	return true;
+}
+/**************************************************************/
+
+/* 获取某个运算符的优先级 *****************************************/
+char * priority[2][2] = {
+	{"+", "-"},
+	{"*", "/"}
+};
+
+int get_priority(char operator){
+	bool check = false;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			if (priority[i][j][0] == operator) {
+				check = true;
+				return i;
+			}
+	Assert(check == true, "操作符的优先级无定义");
+	return 10000;
+}
+/**************************************************************/
+
+
+/* 寻找dominant operator ***************************************/
+// 先实现算数表达式吧, 太复杂了(我太菜了)
+// 至少能通过简单的测试
+int find_dominant_operator(char * sub_expression) {
+	int len = strlen(sub_expression);
+
+	int min_priority = 1000;
+	int index = -1;
+
+	bool in_range = false; // 判断操作符是否在括号内部
+	for (int i = 0; i < len; i++) {
+		char c = sub_expression[i];
+		// 必须是操作符或者括号
+		if (! (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')'))
+			continue;
+		
+		// 若遇到右括号, 则更改当前的in_range状态
+		if (c == ')') {
+			if (in_range)
+				in_range = false;
+			continue;
+		}
+
+		// 若在括号内部
+		if (in_range == true)
+			continue;
+		
+		// 若遇到左括号, 就开启in_range状态
+		if (c == '(') {
+			in_range = true;
+			continue;
+		}
+
+		// 剩余的符号都是操作符
+		if (index == -1) { // 如果还没有赋值
+			index = i;
+			min_priority = get_priority(c);
+		} else if (get_priority(c) <= min_priority) {
+			index = i;
+			min_priority = get_priority(c);
+		}
+	}
+
+	return index;
+}
+/**************************************************************/
+
+/* 求解表达式的值 主体 *******************************************/
+uint32_t get_value(char * expression, int p, int q) {
+	Assert(p <= q, "啊啊啊啊我也不知道怎么办啊");
+	if (p == q) {
+		return expression[p] - '0';
+	}
+	else if (check_parentheses(expression, p, q) == true) {
+		return get_value(expression, p+1, q-1);
+	}
+	else {
+		/* Complicated */
+		// 1. 获取当前子串 记得释放
+		char * sub_expression = sub_str(expression, p, q);
+		// 2. 找出当前子串的dominant operator
+		int d_op_ind = find_dominant_operator(sub_expression);
+		Assert(d_op_ind != -1, "找不到dominant operator!");
+
+
+		// 3. 根据dominant operator求解表达式
+		uint32_t value1 = get_value(expression, p, p + d_op_ind-1);
+		uint32_t value2 = get_value(expression, p + d_op_ind + 1, q);
+
+		Assert(value1 >= value2, "出现负数啦");
+
+		char d_op = sub_expression[d_op_ind];
+		free(sub_expression); // 释放空间
+		switch (d_op) {
+			case '+': {return value1 + value2; break;}
+			case '-': {return value1 - value2; break;}
+			case '*': {return value1 * value2; break;}
+			case '/': {return value1 / value2; break;}
+			default: Assert(0, "取出的操作符不太对劲儿");
+		}
+	}
+}
+/***************************************************************/
 
 uint32_t expr(char *e, bool *success) {
-	// 记得释放tokens中的字符串空间
 
 	// 如果找不到正常的表达式就直接退出
 	if(!make_token(e)) {
@@ -163,7 +395,7 @@ uint32_t expr(char *e, bool *success) {
 
 	/* TODO: Insert codes to evaluate the expression. */
 
-	// 前面已经进行过了make_token
+	// tokens数组已经填充
 
 	// 拼接表达式
 	int expression_size = 100;
@@ -173,10 +405,13 @@ uint32_t expr(char *e, bool *success) {
 	for (int i = 0; i < nr_token; i++)
 		strcat(expression, tokens[i].str);
 
-	printf("%s\n", expression);
+	// printf("%s\n", expression);
 
+	// 计算表达式
+	int len = strlen(expression);
+	uint32_t res = get_value(expression, 0, len-1);
+	printf("%u\n", res);
 	
-
 	// 测试部分, 打印中取出的token
 	// for (int i = 0; i < nr_token; i++){
 	// 	printf("%s ", tokens[i].str);
@@ -186,6 +421,8 @@ uint32_t expr(char *e, bool *success) {
 	// 释放tokens中的字符串空间
 	for (int i = 0; i < nr_token; i++)
 		free(tokens[i].str);
+	// 释放表达式空间
+	free(expression);
 
 	// panic("please implement me");
 	return 0;
