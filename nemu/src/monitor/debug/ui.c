b9cd92dc72e94cde2462f34f7f7aa4a7238a4beb
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 08:51:45 +0800

    >  compile NEMU 3019244317 lxq Linux lxq-KLV-WX9 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:51:45 up  1:39,  1 user,  load average: 0.86, 0.59, 0.58 321d10d22474779c301c08a9f48e0c99074ba90

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -107,6 +107,7 @@ static int cmd_info(char *args) {
 }
 
 static int cmd_x(char *args){
+	printf("Attention! 你需要以字节为单位反过来看");
 	char * first_arg = strtok(args, " ");
 	char * second_arg = strtok(NULL, " ");
 	
@@ -125,7 +126,7 @@ static int cmd_x(char *args){
 	for (int i = 0; i < num; i++) {
 		res = swaddr_read(st_addr, 4);
 		// 输出
-		printf("0x%x  ", res);
+		printf("0x%08x  ", res);
 		if ( (i+1) % 5 == 0)
 			printf("\n");
 		// 更新起始地址
