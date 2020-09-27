6f7465093635aa5fef6ba81df824f5814fbf8f3f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 22:23:31 +0800

    >  compile NEMU 3019244317 lxq Linux lxq-KLV-WX9 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:23:31 up 15:11,  1 user,  load average: 0.67, 0.52, 0.47 85f3de9b80d34649b8dc636d5553d5a0300811db

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -488,6 +488,7 @@ uint32_t get_value(int p, int q) {
 	}
 
 	free(sub_expression);
+	printf("%u\n", ret_val);
     return ret_val;
 }
 /***************************************************************/
