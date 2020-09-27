9cdd4ab24de85bd4321758ae9065de97a513b783
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 08:33:20 +0800

    >  compile NEMU 3019244317 lxq Linux lxq-KLV-WX9 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:33:20 up  1:21,  1 user,  load average: 0.99, 0.64, 0.68 fb513cee26769dac8fd3d561a4fa2ab0933b3b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -125,10 +125,11 @@ static int cmd_x(char *args){
 	for (int i = 0; i < num; i++) {
 		res = swaddr_read(st_addr, 4);
 		// 输出
-		printf("%x  ", res);
+		printf("0x%8x  ", res);
 		// 更新起始地址
 		st_addr = st_addr + 0x4;
 	}
+	printf("\n");
 	return -1;
 }
 
