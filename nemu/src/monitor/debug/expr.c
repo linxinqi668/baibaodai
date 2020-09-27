e5d8b5aef8d3fbc7f15ad591e0bbcd6fb945b4f1
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 01:07:50 +0800

    >  compile NEMU 3019244317 lxq Linux lxq-KLV-WX9 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:07:50 up 32 min,  1 user,  load average: 1.35, 1.08, 0.80 1095bb7bfa4f8d76e887235ab5ff12d489fb4e01

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -468,7 +468,7 @@ uint32_t get_value(int p, int q) {
 	} else if (p == q) { // 只剩一个数字单元
 		if (tokens[p].type == Hex_Num){
 			ret_val = strtol(tokens[p].str, NULL, 10);
-			printf("%u",ret_val);
+			printf("%s",tokens[p].str);
 		}
 		else
 			ret_val = atoi(tokens[p].str);
