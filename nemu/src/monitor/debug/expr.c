94d44d04b7e61b69484057758ecb98eead7149ce
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 22:56:49 +0800

    >  compile NEMU 3019244317 lxq Linux lxq-KLV-WX9 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:56:49 up 15:44,  1 user,  load average: 0.39, 0.49, 0.52 7347e0a888e8547eff92917fc4afe16e913a7705

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -524,7 +524,7 @@ uint32_t expr(char *e, bool *success) {
 	}
 
 	for (i = 1; i < nr_token && (tokens[i].type == Neg); i++){
-		printf("%c\n", tokens[i].type);
+		printf("%c!!!!!!!!!!!!!!!\n", tokens[i].type);
 		if (tokens[i-1].type == Right || tokens[i-1].type == Integer
 		    || tokens[i-1].type == Hex_Num || tokens[i-1].type == Reg_Name){
 			tokens[i].type = Sub; printf("find\n");}
