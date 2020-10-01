0af3cfd1aa6c5a004938a1da869e1a295d59fc2e
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-01 11:06:22 +0800

    >  compile NEMU 3019244317 lxq Linux lxq-KLV-WX9 5.4.0-48-generic #52~18.04.1-Ubuntu SMP Thu Sep 10 12:50:22 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:06:22 up  1:36,  1 user,  load average: 0.61, 0.48, 0.52 f43b48d8754b14c0eebf1ba5c6c5f12ba8a65f1b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -217,6 +217,7 @@ static int cmd_p(char *args){
 	}
 
 	bool * is_valid = (bool *) malloc(1); // 判断是否是合法的表达式
+	printf("%s\nx", args);
 	int res = expr(args, is_valid);
 	if (*is_valid){
 		free(is_valid);
