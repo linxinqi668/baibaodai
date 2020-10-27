8fae76d76aa5f2119fb95f3871b9f2b3e6ee31be
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-27 21:46:45 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:46:45 up 13:12,  1 user,  load average: 0.73, 0.78, 0.67 153a5e134b9ba2c1777f6ad7333a430b5e1e9b8

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -1,11 +1,16 @@
 #include "cpu/exec/template-start.h"
 
 make_helper( concat(call_rel_, SUFFIX) ) {
+    // push address of next instruction.
+    REG(R_ESP) = REG(R_ESP) - DATA_BYTE;
+    MEM_W( REG(R_ESP), eip + 0x5);
+    
     // decode
     DATA_TYPE_S rel = instr_fetch(eip + 1, DATA_BYTE);
 
     // set eip only for 4 byte(i am lazy~)
     cpu.eip = cpu.eip + 1 + DATA_BYTE + rel;
+
 
     // ret
     return 0;
