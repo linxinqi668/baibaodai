7f171b444d2b80eb30ef7024ccea806cf7becc46
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 21:31:32 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:31:32 up  8:47,  1 user,  load average: 0.74, 0.54, 0.43 b95e6ecdc7de906ae0fda8226d0d5d4950609fce

--- nemu/src/cpu/exec/push/push-template.h
+++ nemu/src/cpu/exec/push/push-template.h
@@ -3,12 +3,13 @@
 #define instr push
 
 static void do_execute() {
-    // write src into (esp)
-    MEM_W(R_ESP, op_src->val);
-
     // change the esp
     reg_l(R_ESP) = reg_l(R_ESP) - DATA_BYTE;
+
+    // write src into (esp)
+    MEM_W(reg_l(R_ESP), op_src->val);
 }
+
 
 make_instr_helper(r);
 
