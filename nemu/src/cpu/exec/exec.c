0a33b63c36943425f8881ac2b61404ba7998bb2e
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 02:15:14 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:15:14 up 13:30,  1 user,  load average: 0.94, 1.01, 0.97 568f1df03f155603b9f965616ef69a52ed3bfd9

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -117,7 +117,7 @@ helper_fun opcode_table [256] = {
 /* 0x50 */	push_r_l, inv, inv, push_r_l,
 /* 0x54 */	inv, push_r_l, push_r_l, push_r_l,
 /* 0x58 */	inv, inv, inv, pop_r_v,
-/* 0x5c */	inv, pop_r_l, pop_r_l, inv,
+/* 0x5c */	inv, pop_r_l, pop_r_l, pop_r_l,
 /* 0x60 */	inv, inv, inv, inv,
 /* 0x64 */	inv, inv, operand_size, inv,
 /* 0x68 */	inv, inv, push_i_b, inv,
