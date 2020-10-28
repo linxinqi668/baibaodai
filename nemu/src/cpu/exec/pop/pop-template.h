7613f4bbf039feef4ec30dfe2a3b94d67259c157
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 20:42:07 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:42:07 up  7:57,  1 user,  load average: 0.50, 0.37, 0.48 821f3b48cf786e33444ebde6c918cd8aa9decee

--- nemu/src/cpu/exec/pop/pop-template.h
+++ nemu/src/cpu/exec/pop/pop-template.h
@@ -7,6 +7,10 @@ static void do_execute() {
     DATA_TYPE val = MEM_R( reg_l(R_ESP) );
     OPERAND_W(op_dest, val);
 
+    if (cpu.eip == 0x100053) {
+        printf("dest is: %s", regsl[op_dest->reg]);
+    }
+
     // change the esp.
     reg_l(R_ESP) = reg_l(R_ESP) + DATA_BYTE;
 }
