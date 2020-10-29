9f5cb3ddb04cfafdb1029f164fa5be6caabe9bbb
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 02:11:07 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:11:07 up 1 day, 13:26,  1 user,  load average: 1.15, 0.71, 0.50 3d747fe666be95ea2f0539c51fa89e4bf8076af5

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -11,7 +11,8 @@ char *s[] = {
 };
 
 char str1[] = "Hello";
-char str[20] = "!!!#################";
+//char str[20] = "!!!#################"; // memset到第3个就不行了.
+char str[20];
 
 int main() {
 	//nemu_assert(strcmp(s[0], s[2]) == 0); // OK
