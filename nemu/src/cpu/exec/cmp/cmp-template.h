a33678a624a5f3e2c659c79c530ca5be69e66c15
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 16:46:36 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:46:36 up 1 day,  4:02,  1 user,  load average: 0.81, 0.56, 0.56 a282b16243fdd7caded5a9d5da1cea4ab592b9f

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -54,16 +54,16 @@ static void do_execute() {
         cpu.EFLAGS.CF = 0;
 
     // debug:
-    // if (cpu.eip == 0x1000ac) {
-    //     printf("dest is: %d\n", op_dest->val);
-    //     printf("src is: %d\n", src);
-    //     printf("res is: %d\n", minus_res);
-    //     printf("CF is: %d\n", cpu.EFLAGS.CF);
-    //     printf("ZF is: %d\n", cpu.EFLAGS.ZF);
-    //     printf("OF is: %d\n", cpu.EFLAGS.OF);
-    //     printf("SF is: %d\n\n\n\n\n", cpu.EFLAGS.SF);
-    //     // assert(cpu.EFLAGS.SF == 1);
-    // }
+    if (cpu.eip == 0x100077) {
+        printf("dest is: %d\n", op_dest->val);
+        printf("src is: %d\n", src);
+        printf("res is: %d\n", minus_res);
+        printf("CF is: %d\n", cpu.EFLAGS.CF);
+        printf("ZF is: %d\n", cpu.EFLAGS.ZF);
+        printf("OF is: %d\n", cpu.EFLAGS.OF);
+        printf("SF is: %d\n\n\n\n\n", cpu.EFLAGS.SF);
+        // assert(cpu.EFLAGS.SF == 1);
+    }
 
     // set AF 草, 居然没用到这个标记位.
     
