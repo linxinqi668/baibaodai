6fde9806ff499d0bdf674906512348ac303bc3ce
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 01:18:09 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:18:09 up 1 day, 12:33,  1 user,  load average: 0.29, 0.40, 0.45 4d50cdb565b7ddf6880259f4cc1461549726a2ae

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -31,7 +31,7 @@ make_helper(rep) {
 				 (ops_decoded.opcode == 0xae) || // scab
 				 (ops_decoded.opcode == 0xaf))   // scaw
 			{
-				if (cpu.EFLAGS.ZF == 1 && instr_fetch(eip, 1) == 0xf3)
+				if (cpu.EFLAGS.ZF == 0 && instr_fetch(eip, 1) == 0xf3)
 					break;
 			}
 		}
@@ -64,8 +64,9 @@ make_helper(repnz) {
 			     (ops_decoded.opcode == 0xa7) || // cmpw
 				 (ops_decoded.opcode == 0xae) || // scab
 				 (ops_decoded.opcode == 0xaf))   // scaw
-			{
-				if (cpu.EFLAGS.ZF != 0 && instr_fetch(eip, 1) == 0xf2)
+			{	// not zero -> exec.
+				// zero -> exit
+				if (cpu.EFLAGS.ZF == 1)
 					break;
 			}
 	}
