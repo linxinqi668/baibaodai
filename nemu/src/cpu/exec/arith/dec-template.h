b536a8ca759bbc3a5d94a9fd9b3aa3c73d0610bd
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 01:29:22 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:29:22 up 1 day, 12:44,  1 user,  load average: 0.50, 0.53, 0.49 16dde247b351d9b5b51faa5eaa9414aee0a8cb54

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -8,6 +8,31 @@ static void do_execute () {
 
 	/* TODO: Update EFLAGS. */
 	// panic("please implement me");
+	// set ZF
+    cpu.EFLAGS.ZF = (result == 0) ? 1 : 0;
+
+    // set PF
+    uint8_t low_byte = result;
+    uint32_t count;
+    for (count = 0; low_byte; ++count)
+        low_byte &= (low_byte - 1); // 不断清除右边的1
+    cpu.EFLAGS.PF = (count % 2 == 0) ? 1 : 0;
+
+
+    // set SF
+    cpu.EFLAGS.SF = (DATA_TYPE)result >> (DATA_BYTE * 8 - 1);
+
+    // set OF in subtraction.
+    // 符号不同, 且结果与减数符号相同.
+    if (
+        (!(((DATA_TYPE)result >> (DATA_BYTE * 8 - 1)) ^
+        ((DATA_TYPE)1 >> (DATA_BYTE * 8 - 1)))) &&
+        (((DATA_TYPE)op_src->val >> (DATA_BYTE * 8 - 1)) ^
+        ((DATA_TYPE)1 >> (DATA_BYTE * 8 - 1)))
+    )
+        cpu.EFLAGS.OF = 1;
+    else
+        cpu.EFLAGS.OF = 0;
 
 	print_asm_template1();
 }
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -112,7 +112,7 @@ helper_fun opcode_table [256] = {
 /* 0x3c */	cmp_i2A_b, inv, inv, inv,
 /* 0x40 */	inv, inv, inv, inv,
 /* 0x44 */	inv, inv, inv, inv,
-/* 0x48 */	inv, inv, inv, inv,
+/* 0x48 */	inv, inv, inv, dec_r_v,
 /* 0x4c */	inv, inv, inv, dec_r_v,
 /* 0x50 */	push_r_l, inv, inv, push_r_l,
 /* 0x54 */	inv, push_r_l, push_r_l, push_r_l,
