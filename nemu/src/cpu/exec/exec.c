a18db727e43ad2e92da1cb4674d6b17b4d145477
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 16:27:33 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:27:33 up 1 day,  3:43,  1 user,  load average: 0.52, 0.74, 0.70 ded04f2459b05669c9f84a447be68192d12e682

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -80,7 +80,7 @@ make_group(group4,
 /* 0xff */
 make_group(group5,
 	inv, inv, inv, inv, 
-	inv, jmp_reg_l, push_rm_v, inv)
+	jmp_reg_l, inv, push_rm_v, inv)
 
 make_group(group6,
 	inv, inv, inv, inv, 
