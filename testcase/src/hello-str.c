cc01c70347cec77248ef5629dc1e6cb9ccebf22f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 14:54:51 +0800

    >  run 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:54:51 up 2 days,  2:10,  1 user,  load average: 0.64, 0.73, 0.72 a70d11b0a524fa411b7ccc4ecdbbf9b8fc02a44

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -11,7 +11,7 @@ int main() {
 	// sprintf(buf, "%s", "1"); // 过不去.	
 	// nemu_assert(strcmp(buf, "1") == 0);
 
-	nemu_assert(strcmp("xxxxxx", "xxxxxx") == 0);
+	nemu_assert(strlen("xxxx") == 4);
 
 	// sprintf(buf, "%s", "Hello world!\n"); // 过不去.	
 	// nemu_assert(strcmp(buf, "Hello world!\n") == 0);
