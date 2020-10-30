5cfb57794d34d43193deed6ac4f6cc17d8cc0613
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 15:07:29 +0800

    >  run 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:07:29 up 2 days,  2:22,  1 user,  load average: 1.22, 0.73, 0.64 918aa7614318d2b2fd9d45e441bf5b2a862648ae

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -8,13 +8,13 @@ char buf[128];
 int main() {
 
 	// sprintf(buf, "%s", "0"); // 一个字符都写不进去.
-	sprintf(buf, "%s", "1"); // 过不去.	
-	nemu_assert(strcmp(buf, "1") == 0);
+	// sprintf(buf, "%s", "1"); // 过不去.	
+	// nemu_assert(strcmp(buf, "1") == 0);
 
 	// nemu_assert(strlen("xxxx") == 4);
 
-	// sprintf(buf, "%s", "Hello world!\n"); // 过不去.	
-	// nemu_assert(strcmp(buf, "Hello world!\n") == 0);
+	sprintf(buf, "%s", "Hello world!\n"); // 过不去.	
+	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
 	// sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	// nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
