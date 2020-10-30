7cc1fcb85616f79a8653425731e4dc6e68239734
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 10:55:18 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:55:18 up 1 day, 22:10,  1 user,  load average: 0.37, 0.38, 0.41 174b4d4176a1053d141868c262253ef83704a9f2

--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -11,21 +11,38 @@ static void do_execute () {
 	OPERAND_W(op_dest, dest);
 
 	/* TODO: Update EFLAGS. */
+	// set ZF.
 	cpu.EFLAGS.ZF = (dest == 0) ? 1 : 0;
 
+	// set CF.
 	int temp = count;
 	DATA_TYPE rm = op_dest->val;
-	uint8_t high_order_bit = 
-			(rm >> (DATA_BYTE * 8 - 1) ) & 0x1;
+	uint8_t sign_bit = 0;
 	while (temp) {
 		uint8_t low_order_bit = rm & 0x1;
-		cpu.EFLAGS.CF = low_order_bit;
+		cpu.EFLAGS.CF = low_order_bit; // CF = 移入的数值.	
 		rm = rm / 2; // unsigned div.
 		temp--;
+		// 保存倒数第二次移位后的符号位.
+		if (temp == 1)
+			sign_bit = (rm >> (DATA_BYTE * 8 - 1) ) & 0x1;
 	}
 
-	if (count == 1)
-		cpu.EFLAGS.OF = high_order_bit;
+	// set OF.
+	uint8_t sign_bit_last = (rm >> (DATA_BYTE * 8 - 1) ) & 0x1;
+	if (count == 1) // 不同的话OF就是1.
+		cpu.EFLAGS.OF = sign_bit ^ sign_bit_last;
+
+	// set SF.
+	cpu.EFLAGS.SF = (sign_bit_last == 1) ? 1 : 0;
+
+	// set PF
+    uint8_t low_byte = dest;
+    uint32_t cnt;
+    for (cnt = 0; low_byte; ++cnt)
+        low_byte &= (low_byte - 1); // 不断清除右边的1
+    cpu.EFLAGS.PF = (cnt % 2 == 0) ? 1 : 0;
+	
 
 	print_asm_template2();
 }
