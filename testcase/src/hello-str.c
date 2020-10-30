d81bd58f64c09050a21c125b1a9b7d54c324bb28
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 14:41:01 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:41:01 up 2 days,  1:56,  1 user,  load average: 0.77, 0.68, 0.71 a73498a564eacd7342f4c12599d046ad97e587e3

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -6,10 +6,10 @@ char buf[128];
 
 int main() {
 
-	sprintf(buf, "%s", "0"); // 一个字符都写不进去.
-	// sprintf(buf, "%s", "Hello world!\n"); // 过不去.
+	// sprintf(buf, "%s", "0"); // 一个字符都写不进去.
+	sprintf(buf, "%s", "Hello world!\n"); // 过不去.
 	
-	// nemu_assert(strcmp(buf, "Hello world!\n") == 0);
+	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
 	// sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
 	// nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
