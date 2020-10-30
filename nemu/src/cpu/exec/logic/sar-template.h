e906287b592e569912e3655a3e4131cd2906d126
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 11:03:57 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:03:57 up 1 day, 22:19,  1 user,  load average: 0.71, 0.62, 0.50 4dd078c3b2c11fdcd33c68264d401840c77267d5

--- nemu/src/cpu/exec/logic/sar-template.h
+++ nemu/src/cpu/exec/logic/sar-template.h
@@ -11,6 +11,10 @@ static void do_execute () {
 	OPERAND_W(op_dest, dest);
 
 	/* TODO: Update EFLAGS. */
+	// set ZF.
+	cpu.EFLAGS.ZF = (dest == 0) ? 1 : 0;
+
+	// set CF.
 	int temp = count;
 	DATA_TYPE_S rm = op_dest->val;
 	while (temp) {
@@ -20,8 +24,20 @@ static void do_execute () {
 		temp--;
 	}
 
+	// set OF.
 	if (count == 1)
 		cpu.EFLAGS.OF = 0;
+
+	// set SF.
+	uint8_t sign_bit_last = (rm >> (DATA_BYTE * 8 - 1) ) & 0x1;
+	cpu.EFLAGS.SF = (sign_bit_last == 1) ? 1 : 0;
+
+	// set PF
+    uint8_t low_byte = dest;
+    uint32_t cnt;
+    for (cnt = 0; low_byte; ++cnt)
+        low_byte &= (low_byte - 1); // 不断清除右边的1
+    cpu.EFLAGS.PF = (cnt % 2 == 0) ? 1 : 0;
 
 	print_asm_template2();
 }
