085b651602de0ce9278367d8c96ae379dba454e9
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 11:01:39 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:01:39 up 1 day, 22:17,  1 user,  load average: 0.91, 0.63, 0.49 9295efa2f0b8da0b94a12d58d812288b825a9cf2

--- nemu/src/cpu/exec/logic/shl-template.h
+++ nemu/src/cpu/exec/logic/shl-template.h
@@ -11,19 +11,36 @@ static void do_execute () {
 	OPERAND_W(op_dest, dest);
 
 	/* TODO: Update EFLAGS. */
+	// set ZF.
+	cpu.EFLAGS.ZF = (dest == 0) ? 1 : 0;
+
+	// set CF.
 	int temp = count;
 	DATA_TYPE_S rm = op_dest->val;
+	uint8_t old_high_order_bit = 
+			(rm >> (DATA_BYTE * 8 - 1) ) & 0x1;
 	while (temp) {
 		uint8_t high_order_bit = 
 			(rm >> (DATA_BYTE * 8 - 1) ) & 0x1;
-		cpu.EFLAGS.CF = high_order_bit;
+		cpu.EFLAGS.CF = high_order_bit; // 等于移入的数值.
 		rm = rm * 2;
 		temp--;
 	}
 
+	// set OF.
 	if (count == 1)
-		cpu.EFLAGS.OF = 
-		((rm >> (DATA_BYTE * 8 - 1) ) & 0x1) != cpu.EFLAGS.CF;
+		cpu.EFLAGS.OF = (old_high_order_bit != cpu.EFLAGS.CF);
+
+	// set SF.
+	// CF is the hight order bit.
+	cpu.EFLAGS.SF = (cpu.EFLAGS.CF == 1) ? 1 : 0;
+	
+	// set PF
+    uint8_t low_byte = dest;
+    uint32_t cnt;
+    for (cnt = 0; low_byte; ++cnt)
+        low_byte &= (low_byte - 1); // 不断清除右边的1
+    cpu.EFLAGS.PF = (cnt % 2 == 0) ? 1 : 0;
 
 	print_asm_template2();
 }
