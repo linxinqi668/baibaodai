46911ea22ee5b299c499275c22c0e98a4ca9a6c3
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-02 01:03:31 +0800

    >  run 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:03:31 up  7:50,  1 user,  load average: 0.79, 0.46, 0.41 46641766e932c9615772f961d4a75d35511fd96

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -6,27 +6,27 @@ FLOAT f(FLOAT x) {
 	return F_div_F(int2F(1), int2F(1) + F_mul_int(F_mul_F(x, x), 25));
 }
 
-// FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
-// 	int k;
-// 	FLOAT s,h;
-// 	h = F_div_int((b - a), n); // 步长.
-// 	s = F_div_int(fun(a) + fun(b), 2 );
-// 	for(k = 1; k < n; k ++) {
-// 		s += fun(a + F_mul_int(h, k));
-// 	}
-// 	s = F_mul_F(s, h);
-// 	return s;
-// }
+FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
+	int k;
+	FLOAT s,h;
+	h = F_div_int((b - a), n); // 步长.
+	s = F_div_int(fun(a) + fun(b), 2 );
+	for(k = 1; k < n; k ++) {
+		s += fun(a + F_mul_int(h, k));
+	}
+	s = F_mul_F(s, h);
+	return s;
+}
 
 int main() { 
-	// FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f); // f2F没问题.
-	// FLOAT ans = f2F(0.551222);
+	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f); // f2F没问题.
+	FLOAT ans = f2F(0.551222);
 	// vnemu_assert(Fabs(ans - a) < f2F(100.0))
 
-	FLOAT res = f(f2F(0.1));
+	// FLOAT res = f(0);
 
-	// nemu_assert(Fabs(a - ans) < f2F(1e-2));
-	nemu_assert(Fabs(res - f2F(0.8)) < f2F(1e-1));
+	nemu_assert(Fabs(a - ans) < f2F(1e-2));
+	// nemu_assert(Fabs(res - 0) < f2F(2.0));
 
 	return 0;
 }
