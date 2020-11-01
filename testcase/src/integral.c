33b5417b7be537b4f068e0adf13df7d65ab2330b
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-02 00:03:14 +0800

    >  run 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:03:14 up  6:50,  1 user,  load average: 0.66, 0.62, 0.69 9b01cc02a198741b82c80e78fc2e4de0dbdc1dc0

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -6,17 +6,17 @@ FLOAT f(FLOAT x) {
 	return F_div_F(int2F(1), int2F(1) + F_mul_int(F_mul_F(x, x), 25));
 }
 
-// FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
-// 	int k;
-// 	FLOAT s,h;
-// 	h = F_div_int((b - a), n);
-// 	s = F_div_int(fun(a) + fun(b), 2 );
-// 	for(k = 1; k < n; k ++) {
-// 		s += fun(a + F_mul_int(h, k));
-// 	}
-// 	s = F_mul_F(s, h);
-// 	return s;
-// }
+FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
+	int k;
+	FLOAT s,h;
+	h = F_div_int((b - a), n);
+	s = F_div_int(fun(a) + fun(b), 2 );
+	for(k = 1; k < n; k ++) {
+		s += fun(a + F_mul_int(h, k));
+	}
+	s = F_mul_F(s, h);
+	return s;
+}
 
 int main() { 
 	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
