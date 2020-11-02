58b321a168541d39e930a342a130e9e26690f43c
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 01:40:00 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:40:00 up  3:27,  1 user,  load average: 0.40, 0.35, 0.36 904dad5e29cca8c76aaf9d3a9c137f0b02c3aa1

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -36,7 +36,7 @@ char * get_fun_name(swaddr_t addr) {
 					printf("reached this line3\n");
 					printf("fun len is : %d\n", fun_len);
 					printf("bias 1 is : %d\n", symtab[i].st_name);
-					printf("bias 2 is : %d\n", symtab[i+1].st_name);
+					printf("bias 2 is : %d\n", symtab[i-1].st_name);
 					// 取出函数名.
 					strncpy(fun_name, strtab + symtab[i].st_name, fun_len);
 					fun_name[fun_len] = '\0';
