66c950fb33e4869a9ea3dafc44620352253e3dce
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-02 23:22:16 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:22:16 up  1:09,  1 user,  load average: 0.64, 0.52, 0.49 59bd40cc44e551f6ade64e428b063a9536c0ceaa

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -93,7 +93,7 @@ void load_elf_tables(int argc, char *argv[]) {
 	free(shstrtab);
 
 	assert(strtab != NULL && symtab != NULL);
-	// printf("%x", &(*strtab));
+	printf("%c", *strtab);
 
 	fclose(fp);
 }
