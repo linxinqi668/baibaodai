a3d74f1942c69627fb61ace6cba9a7925f1f3452
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-02 23:12:48 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:12:48 up  1:00,  1 user,  load average: 0.64, 0.50, 0.43 e60523cb30a75da44208511371c657e291b4257

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -516,6 +516,7 @@ uint32_t get_variable_addr(char * var_name) {
 	bool find = false;
 
 	printf("%s\n", var_name);
+	printf("%d\n", len_var);
 	for (i = 0; i <= len_strlab - len_var; i += len_var)
 		if (strcmp(strtab + i, var_name) == 0) {
 			find = true;
