f52422026a558c36acf9bf60021760c68d31db2f
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 00:04:27 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:04:27 up  1:52,  1 user,  load average: 0.75, 0.58, 0.52 dc09bcec65fec5fb39f593a72652bb805870608a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -519,8 +519,8 @@ uint32_t get_variable_addr(char * var_name) {
 	Elf32_Sym * symtab = get_symtab();
 	int len_symtab = get_symtab_len();
 	for (i = 0; i < len_symtab; i++) {
-		printf("%d\n", symtab[i].st_info);
-		// printf("%x\n", symtab[i].st_value);
+		// printf("%d\n", symtab[i].st_info);
+		printf("%x\n", symtab[i].st_value);
 		if (symtab[i].st_info == STT_OBJECT) { // 如果是OBJECT.
 			// 比对str.
 			printf("here\n");
