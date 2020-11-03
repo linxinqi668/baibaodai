98bcbe7223d1939ba21ffdec741b37362a81995c
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 22:50:56 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:50:56 up 1 day, 38 min,  1 user,  load average: 0.54, 0.86, 0.89 512beeb231818bdd2c2c7a38b04dcafcaa0bdd70

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -10,35 +10,26 @@ char *s[] = {
 	"#####"
 };
 
-// 内存读取不太对劲.
-char str2[] = "1234567890";
-
-char str1[] = "11111111111111111111111111111111111111111111111\
-			   AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\
-			   BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
+char str1[] = "Hello";
 //char str[20] = "!!!#################"; // memset到第3个就不行了.
-char str[400];
+char str[20];
 
 int main() {
-	// nemu_assert(strcmp(s[0], "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") == 0); // OK
-	//nemu_assert(strcmp(s[0], s[1]) == -1); // OK
-	// nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1); // OK
-	// nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1); // OK
-	// nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1); // OK
+	nemu_assert(strcmp(s[0], s[2]) == 0); // OK
+	nemu_assert(strcmp(s[0], s[1]) == -1); // OK
+	nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1); // OK
+	nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1); // OK
+	nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1); // OK
 
-	// nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0); // OK
+	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0); // OK
 
 	// memcmp没问题. 是memset的问题.
 	// stos的操作数大小设置错了.
-	// nemu_assert(memcmp(/*"#####"*/memset(str, '#', 5), s[5], 5) == 0);
+	nemu_assert(memcmp(/*"#####"*/memset(str, '#', 5), s[5], 5) == 0);
 
 	// nemu_assert(memcmp("12345678910", "12345678910", 11) == 0);
 
-	// memset(str, 0, 50);
-	// nemu_assert(strcmp(memset(str, '#', 5), s[5]) == 0);
-
-	memcpy(str, str1, 155);
-	nemu_assert(strcmp(str1, str) == 0);
-
+	memset(str, 0, 50);
+	nemu_assert(strcmp(memset(str, '#', 5), s[5]) == 0);
 	return 0;
 }
