99e66eb010b39256c96500a2fc578adf0a8e9a5a
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 18:47:56 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:47:56 up 20:35,  1 user,  load average: 1.08, 0.94, 0.94 7586cef63c6fbd9156068cd5a978d5fab21b44c

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -79,9 +79,9 @@ make_helper(concat(cmp_i2A_, SUFFIX)) {
     // decode. eip -> imm
     int len = concat(decode_i_, SUFFIX)(eip + 1);
 
-    DATA_TYPE src = op_src->val;
-    DATA_TYPE dest = REG(R_EAX);
-    DATA_TYPE minus_res = dest - src;
+    DATA_TYPE_S src = op_src->val;
+    DATA_TYPE_S dest = REG(R_EAX);
+    DATA_TYPE_S minus_res = dest - src;
 
     // set ZF
     cpu.EFLAGS.ZF = (minus_res == 0) ? 1 : 0;
