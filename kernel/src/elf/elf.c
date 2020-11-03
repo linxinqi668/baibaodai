e4ef4b371c2390301316e52d145431e4c5a0cbb9
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 19:32:53 +0800

    >  compile kernel 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:32:53 up 21:20,  1 user,  load average: 1.06, 1.06, 0.99 1a06f26e12e4a083d73bf8c73ca2c76595f48fb

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -73,7 +73,8 @@ uint32_t loader() {
 			// 3. 根据offset找到节的内容.
 			// 3. 每个文件的开头都是0x0. 读取前4个字节就是魔数.
 			// 仿照另一个elf.c文件就可以写出来了.
-			uint8_t * st_addr = (uint8_t *)ph->p_paddr;
+			uint8_t * st_addr = (uint8_t *)ph->p_vaddr;
+			printf("%x\n", st_addr);
 			uint32_t mem_size = ph->p_memsz;
 			uint32_t file_size = ph->p_filesz;
 			uint32_t offset = ph->p_offset;
