7619fda689b03bbd7c1b806fbbff46d0ae511898
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 18:51:56 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:51:56 up 20:39,  1 user,  load average: 0.90, 0.87, 0.91 af1d306d212b931db53f0b36b3deac93acb69c1

--- nemu/src/cpu/exec/cmp/cmp-template.h
+++ nemu/src/cpu/exec/cmp/cmp-template.h
@@ -83,6 +83,19 @@ make_helper(concat(cmp_i2A_, SUFFIX)) {
     DATA_TYPE_S dest = REG(R_EAX);
     DATA_TYPE_S minus_res = dest - src;
 
+    // debug:
+    if (cpu.eip == 0x1009e3) {
+        printf("dest is: %x\n", op_dest->val);
+        printf("src is: %x\n",  src);
+        printf("op size is: %d\n", (int)op_src->size);
+        printf("res is: %d\n", minus_res);
+        printf("CF is: %d\n", cpu.EFLAGS.CF);
+        printf("ZF is: %d\n", cpu.EFLAGS.ZF);
+        printf("OF is: %d\n", cpu.EFLAGS.OF);
+        printf("SF is: %d\n\n\n\n\n", cpu.EFLAGS.SF);
+        // assert(cpu.EFLAGS.SF == 1);
+    }
+
     // set ZF
     cpu.EFLAGS.ZF = (minus_res == 0) ? 1 : 0;
 
@@ -111,19 +124,6 @@ make_helper(concat(cmp_i2A_, SUFFIX)) {
         cpu.EFLAGS.CF = 1;
     else
         cpu.EFLAGS.CF = 0;
-
-    // debug:
-    if (cpu.eip == 0x1009e3) {
-        printf("dest is: %x\n", op_dest->val);
-        printf("src is: %x\n",  src);
-        printf("op size is: %d\n", (int)op_src->size);
-        printf("res is: %d\n", minus_res);
-        printf("CF is: %d\n", cpu.EFLAGS.CF);
-        printf("ZF is: %d\n", cpu.EFLAGS.ZF);
-        printf("OF is: %d\n", cpu.EFLAGS.OF);
-        printf("SF is: %d\n\n\n\n\n", cpu.EFLAGS.SF);
-        // assert(cpu.EFLAGS.SF == 1);
-    }
 
     return 1 + len;
 }
