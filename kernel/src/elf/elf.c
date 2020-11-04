27e9df142f7698c8fb5473c5b5051cdceea9d273
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-04 22:24:52 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:24:52 up 2 days, 12 min,  1 user,  load average: 0.83, 0.31, 0.22 45113e7e84a5e9c0f409744f794455da442c4b5e

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -18,26 +18,19 @@ void create_video_mapping();
 uint32_t get_ucr3();
 
 uint32_t loader() {
-	// int test = 0;
-	// nemu_assert(test == 1); // reached this line.
 
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL; // 程序头数组
 
-	// int test = 0;
-	// nemu_assert(test == 1); // reached this line.
-
-	uint8_t buf[8192 * 50];
-
-	// int test = 0;
-	// nemu_assert(test == 1);
+	uint8_t buf[4096];
 
 #ifdef HAS_DEVICE
 	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #else
-	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 8192 * 50); // 已经读取了4096个字节了.
+	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096); // 已经读取了4096个字节了.
 #endif
 
+	// buf 暂时没用. 不知道后面的PA是否有用.
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
@@ -51,9 +44,11 @@ uint32_t loader() {
 
 	// (1) 取出程序头表的偏移(地址).
 	uint32_t ph_offset = elf->e_phoff;
-	// (2) 更新ph.
-	ph = (Elf32_Phdr *)(buf + ph_offset);
-	// (3) 取出程序头的个数.
+	// (2) 取出ELF文件.
+	uint8_t * elf_file = (uint8_t *)0x0;
+	// (3) 更新ph. elf文件就在0内存处.
+	ph = (Elf32_Phdr *)(elf_file + ph_offset);
+	// (4) 取出程序头的个数.
 	uint32_t ph_num = elf->e_phnum;
 
 	// debug.
@@ -78,8 +73,8 @@ uint32_t loader() {
 			// 仿照另一个elf.c文件就可以写出来了.
 			uint8_t * st_addr = (uint8_t *)ph->p_vaddr;
 			
-			// 控制内存范围.
-			nemu_assert(0x800000 <= (uint32_t)st_addr && (uint32_t)st_addr <= 0x900000);
+			// debug.
+			// nemu_assert(0x800000 <= (uint32_t)st_addr && (uint32_t)st_addr <= 0x900000);
 			uint32_t mem_size = ph->p_memsz;
 			uint32_t file_size = ph->p_filesz;
 			uint32_t offset = ph->p_offset;
@@ -97,7 +92,7 @@ uint32_t loader() {
 			// }
 
 			// 写入内存.
-			memcpy(st_addr, buf + offset, file_size);
+			memcpy(st_addr, elf_file + offset, file_size);
 			
 			
 			/* TODO: zero the memory region 
