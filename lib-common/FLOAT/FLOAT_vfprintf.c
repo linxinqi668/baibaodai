6ca487f94425de3bc4d512ec6d939f07c0478898
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-05 01:23:23 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  01:23:23 up 2 days,  3:10,  1 user,  load average: 0.38, 0.43, 0.45 7e04aa182d55a13cbcd1ba5389c27e789ffce9f

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -80,9 +80,9 @@ static void modify_vfprintf() {
 	 uint_fast32_t * addr_rel = (uint_fast32_t *)(addr_call + 1);
 	 printf("addr of _fpmaxtostr is: %x\n", _fpmaxtostr);
 	 printf("addr of rel is: %x\n", addr_rel);
-	 uint_fast32_t old_rel = *addr_rel;
 	 printf("old rel is: %x\n", old_rel);
 	 printf("addr of format is: %x\n", format_FLOAT);
+	 uint_fast32_t old_rel = *addr_rel;
 	 uint_fast32_t new_rel = old_rel -
 	 						 (uint_fast32_t)_fpmaxtostr +
 							 (uint_fast32_t)format_FLOAT;
