1ae7316e2daec1712b6b63a36bdaec532e7bffef
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-06 15:52:47 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-52-generic #57~18.04.1-Ubuntu SMP Thu Oct 15 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:52:47 up 3 days, 17:40,  1 user,  load average: 1.25, 1.20, 1.14 244f87207d54236435716a34351eb2f8edb264fd

--- nemu/src/cpu/exec/sub/sub.h
+++ nemu/src/cpu/exec/sub/sub.h
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-28 12:50:47
+ * @LastEditTime: 2020-11-06 15:47:26
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /NEMU2020/nemu/src/cpu/exec/sub/sub.h
+ */
 
 make_helper(sub_ib2rm_w);
 make_helper(sub_ib2rm_l);
@@ -6,3 +14,6 @@ make_helper(sub_ib2rm_v);
 make_helper(sub_rm2r_v);
 make_helper(sub_r2rm_v);
 make_helper(sub_i2rm_v);
+
+
+make_helper(sub_i2A_v);
