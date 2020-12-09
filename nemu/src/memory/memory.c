011295fbb03c25dbacb6394bb0bd7f5a74dae208
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-09 22:56:48 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:56:48 up 1 day,  1:07,  1 user,  load average: 2.10, 1.19, 0.93 228609409d284a029f7598cdb75c9c31352321ba

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -14,7 +14,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	uint32_t cache_ans = M_CACHE.m_cache_read(&M_CACHE, addr, len);
 	printf("ans: %x cache: %x\n\n\n", answer, cache_ans);
 	// assert(answer == cache_ans);
-	return answer;
+	return cache_ans;
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
