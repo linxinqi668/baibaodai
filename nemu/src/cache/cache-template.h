fcb7f55d33c22538ec02b5125c09d1bcf8d5498f
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-09 23:15:02 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:15:02 up 1 day,  1:25,  1 user,  load average: 0.85, 0.95, 0.95 a6df66f064cdbf72ca0b9c4088f6651a9468353

--- nemu/src/cache/cache-template.h
+++ nemu/src/cache/cache-template.h
@@ -143,7 +143,7 @@ uint32_t cache_read(Cache* cache, uint32_t addr, size_t len) {
     //    printf("len_1: %d len_2: %d\n", (int)len_1, (int)len_2);
 #endif
         unalign* p1 = align_read(cache, addr); // low bit.
-        unalign* p2 = align_read(cache, addr + len_1);
+        unalign* p2 = align_read(cache, addr + len_1 - 1);
         result = (unalign_rw_helper(p1, len_1) << (len_2 * 8)) // shift to store p2.
                  + unalign_rw_helper(p2, len_2);
     } else {
