0424a74ffe4fc7c44489855dc6644a151dfc0d42
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-09 16:41:21 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:41:21 up 18:51,  1 user,  load average: 0.67, 0.47, 0.48 584258d0f99797695117e3a5a605cbd2d9736a6

--- nemu/src/memory/cache-template.h
+++ nemu/src/memory/cache-template.h
@@ -1,5 +1,7 @@
 
 #include <stdlib.h>
+#include <macro.h>
+#include <common.h>
 
 // do some computation.
 #define BLOCK_SIZE (1 << BLOCK_BIT)
@@ -23,10 +25,10 @@ typedef struct cache{
     bool (*m_find) (struct cache* cache, uint32_t addr);
 
     /* read cache */
-    char (*m_cache_read) (struct cache* cache, uint32_t addr);
+    uint32_t (*m_cache_read) (struct cache* cache, uint32_t addr, size_t len);
 
     /* write cache */
-    void (*m_cache_write) (struct cache* cache, uint32_t addr);
+    void (*m_cache_write) (struct cache* cache, uint32_t addr, uint32_t data, size_t len);
 
     /* initialize the cache */
     void (*m_init) (struct cache* cache);
@@ -46,6 +48,7 @@ void init_cache(Cache* cache) {
             cache->m_set[i][j].is_valid = false;
 }
 
+/* 判断缓存是否命中 */
 int find(Cache* cache, uint32_t addr) {
     // 解析地址
     uint32_t tag = addr >> (SET_INDEX_BIT + BLOCK_BIT);
@@ -60,12 +63,13 @@ int find(Cache* cache, uint32_t addr) {
     return -1; // 返回-1表示没找到
 }
 
-char cache_read(Cache* cache, uint32_t addr) {
+/* 底层接口 处理一定对齐的数据 */
+unalign* align_read(Cache* cache, uint32_t addr) {
     // 解析地址
     uint32_t tag = addr >> (SET_INDEX_BIT + BLOCK_BIT);
     uint32_t set_ind = addr << (TAG_BIT) >> (TAG_BIT + BLOCK_BIT);
     uint32_t block_ind = addr << (32 - BLOCK_BIT) >> (32 - BLOCK_BIT);
-    
+
     // 判断是否存在该块
     int line_ind = find(cache, addr);
     bool is_exist = (line_ind == -1) ? false : true;
@@ -75,10 +79,11 @@ char cache_read(Cache* cache, uint32_t addr) {
         int i;
         // 随机选取一行进行替换
         line_ind = rand() % LINE_PER_SET;
-        uint32_t byte_addr = addr >> BLOCK_BIT << BLOCK_BIT; // 块的起始地址
-        // read a block
+        uint32_t byte_addr = addr >> BLOCK_BIT << BLOCK_BIT;
+        
+        // read a block. this performance can be proved.
         for (i = 0; i < BLOCK_SIZE; i++, byte_addr++) {
-            // read a byte ??? 为啥不能调用
+            // read a byte
             char data = dram_read(byte_addr, 1);
             // wirte it into the cache block.
             cache->m_set[set_ind][line_ind].m_block[i] = data;
@@ -88,7 +93,61 @@ char cache_read(Cache* cache, uint32_t addr) {
         cache->m_set[set_ind][line_ind].m_tag = tag;
     }
 
-    return cache->m_set[set_ind][line_ind].m_block[block_ind];
+    char* data_addr = cache->m_set[set_ind][line_ind].m_block + block_ind;
+    return (unalign*)data_addr;
+}
+
+/* unalign 是一个结构体
+ * 我觉得使用方式就是：
+ * 1. 检测是否会越界
+ * 2. 如果会越界就分成两部分，两部分都是相同的操作
+ * 3. 这边是利用了：结构体指针指向哪里，哪里的数据就会被当做结构体
+ */
+
+/* 辅助函数 */
+uint32_t unalign_rw_helper(unalign* addr, size_t len) {
+    char c = len;
+    switch (c) {
+        case 1:
+            return unalign_rw(addr, 1);
+        case 2:
+            return unalign_rw(addr, 2);
+        case 3:
+            return unalign_rw(addr, 3);
+        case 4:
+            return unalign_rw(addr, 4);
+        default:
+            printf("you get here.. it's not a good thing.\n");
+            return -1;
+            break;
+    }
+}
+
+/* read len bytes from cache */
+/* 这可能跨越block */
+uint32_t cache_read(Cache* cache, uint32_t addr, size_t len) {
+    // 准备结果
+    uint32_t result = 0;
+
+    // 判断读取是否对齐
+    uint32_t addr_st = addr >> BLOCK_BIT << BLOCK_BIT; // 块的起始地址
+    uint32_t addr_ed = addr_st + ((-1) >> (32 - BLOCK_BIT)); // 块的终止地址
+    bool is_unalign = (addr_ed < addr + len) ? true : false;
+
+    if (is_unalign) {
+        size_t len_1 = addr_ed - addr + 1;
+        size_t len_2 = len - len_1;
+        unalign* p1 = align_read(cache, addr);
+        unalign* p2 = align_read(cache, addr + len_1);
+        result = (unalign_rw_helper(p1, len_1) << (len_2 * 8)) // shift to store p2
+                 + unalign_rw_helper(p2, len_2);
+    } else {
+        // 对齐了就直接读取
+        unalign* data = align_read(cache, addr);
+        result = unalign_rw_helper(data, len);
+    }
+    
+    return result;
 }
 
 
