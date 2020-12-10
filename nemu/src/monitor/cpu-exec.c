878970f267e46fff5d97aa33191f995a76a595b6
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-10 21:43:30 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:43:30 up 1 day, 23:53,  1 user,  load average: 0.90, 0.76, 0.69 9010065ef29f1a89b5154e1570361daf12a1c617

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -28,47 +28,47 @@ char* running_message [] = {
 	// PART 1
 	"The Road Not Taken 《少有人走的路》\n\n",
 	"Two roads diverged in a yellow wood,\n\
-黄色的树林里分出两条路，黄色的树林里分出两条路，\n",
+黄色的树林里分出两条路，黄色的树林里分出两条路，\n\n",
 	"And sorry I could not travel both\n\
-可惜我不能同时去涉足，\n",
+可惜我不能同时去涉足，\n\n",
 	"And be one traveler,long I stood\n\
-我在那路口久久伫立，\n",
+我在那路口久久伫立，\n\n",
 	"And looked down one as far as I could\n\
-我向着一条路极目望去，\n",
+我向着一条路极目望去，\n\n",
 	"To where it bent in the undergrowth;\n\
-直到它消失在丛林深处。\n",
+直到它消失在丛林深处。\n\n",
 
 	// PART 2
 	"Then took the other, as just as fair,\n\
 And having perhaps the better claim,\n\
 Because it was grassy and wanted wear;\n\
-它荒草萋萋，十分幽寂，显得更诱人、更美丽，\n",
+它荒草萋萋，十分幽寂，显得更诱人、更美丽，\n\n",
 	"Though as for that the passing there\n,\
-虽然在这两条小路上，\n",
+虽然在这两条小路上，\n\n",
 	"Had worn them really about the same,\n\
-都很少留下旅人的足迹，\n",
+都很少留下旅人的足迹，\n\n",
 
 	// PART 3
 	"And both that morning equally lay\n\
 In leaves no step had trodden black.\n\
-虽然那天清晨落叶满地，两条路都未经脚印污染。\n",
+虽然那天清晨落叶满地，两条路都未经脚印污染。\n\n",
 	"Oh, I kept the first for another day!\n\
-呵，留下一条路等改日再见！\n",
+呵，留下一条路等改日再见！\n\n",
 	"Yet knowing how way leads on to way,\n\
-但我知道路径延绵无尽头，\n",
+但我知道路径延绵无尽头，\n\n",
 	"I doubted if I should ever come back.\n\
-恐怕我难以再回返。\n",
+恐怕我难以再回返。\n\n",
 
 	 // PART 4
 	"I shall be telling this with a sigh\n\
 Somewhere ages and ages hence:\n\
-也许多少年后在某个地方，我将轻声叹息把往事回顾\n",
+也许多少年后在某个地方，我将轻声叹息把往事回顾\n\n",
 	"Two roads diverged in a wood, and I —\n\
-一片树林里分出两条路，\n",
+一片树林里分出两条路，\n\n",
 	"I took the one less traveled by,\n\
-而我选了人迹更少的一条，\n",
+而我选了人迹更少的一条，\n\n",
 	"And that has made all the difference.\n\
-从此决定了我一生的道路。\n"
+从此决定了我一生的道路。\n\n"
 
 };
 
