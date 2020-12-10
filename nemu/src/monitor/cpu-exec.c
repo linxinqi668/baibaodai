ac5b1b178b341412ba92a8805f92e431db70a1ad
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-10 22:14:07 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:14:07 up 2 days, 24 min,  1 user,  load average: 0.88, 0.89, 0.87 7bcc3b192abb11303fa1be3c98f75856d3ffd581

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -27,7 +27,7 @@ char asm_buf[128];
 jmp_buf jbuf;
 
 void print(char* str) {
-	printf("\033[1m\033[45;33m HELLO_WORLD \033[0m\n");
+	printf("\033[1m\033[45;33m%s\033[0m\n", str);
 }
 
 char* running_message [] = {
