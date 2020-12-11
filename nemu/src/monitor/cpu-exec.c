0bca65584fe762c55a4f9cfd8bd75cb68fe1cf35
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-11 18:01:38 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:01:38 up 6 min,  1 user,  load average: 1.09, 0.93, 0.44 1ed64445489415ea1a287ebf25a9622e5db7e53c

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -123,8 +123,7 @@ void cpu_exec(volatile uint32_t n) {
 				message_ind++;
 			}
 			else
-				fputs("         ....if you see this, it means nmeu is too slow.....\n",
-				      stderr);
+				print("           ....END....");
 		}
 #endif
 
