739218add8cc8399d9ffca0500e4c9e201eb0599
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-11 17:58:35 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-56-generic #62~18.04.1-Ubuntu SMP Tue Nov 24 10:07:50 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:58:35 up 3 min,  1 user,  load average: 1.31, 0.60, 0.24 6a9cb3879f700856a4386f40ddd951628c98559a

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -35,7 +35,7 @@ char* running_message [] = {
 	"                     The Road Not Taken 《少有人走的路》\n\n\
                                           ---ROBERT FROST\n\n",
 	"                 Two roads diverged in a yellow wood,\n\
-             黄色的树林里分出两条路，黄色的树林里分出两条路，\n\n",
+                       黄色的树林里分出两条路，\n\n",
 	"                  And sorry I could not travel both,\n\
                         可惜我不能同时去涉足，\n\n",
 	"                   And be one traveler,long I stood,\n\
