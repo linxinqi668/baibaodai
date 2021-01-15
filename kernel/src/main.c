0e85b3ad60b29e676642a83e0ded5fa2f828be52
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 03:37:50 +0800

    >  compile kernel 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  03:37:50 up 14:04,  1 user,  load average: 0.70, 0.72, 0.82 863d6b7380867b562099e1be12c7d81c9ecc3c14

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -89,7 +89,7 @@ void init_cond() { // 跳不到这个函数.
 
 	/* Load the program. */
 	uint32_t eip = loader();
-	eip = 0;
+	eip = -10;
 	
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
