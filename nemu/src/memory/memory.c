36274b42c96b0976ce3d8d390908b0fccbe03413
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 02:31:40 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  02:31:40 up 12:58,  1 user,  load average: 1.47, 1.13, 1.04 d04230719bbec19a399f6482784efaef808140

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -49,12 +49,12 @@ hwaddr_t page_translate(lnaddr_t addr){
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	uint32_t answer = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	// uint32_t answer = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	uint32_t cache_ans = L1_M_CACHE.m_cache_read(&L1_M_CACHE, addr, len);
-	printf("ans: %x cache: %x\n\n\n", answer, cache_ans);
-	assert(answer == cache_ans);
-	return cache_ans;
+	// uint32_t cache_ans = L1_M_CACHE.m_cache_read(&L1_M_CACHE, addr, len);
+	// printf("ans: %x cache: %x\n\n\n", answer, cache_ans);
+	// assert(answer == cache_ans);
+	// return cache_ans;
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
