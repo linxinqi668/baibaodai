5350ef145fd29c9a8d77c69efd4fd4f6f1efc9e7
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 04:59:44 +0800

    >  compile kernel 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:59:44 up 15:26,  1 user,  load average: 1.29, 1.14, 1.14 948f11f83563c4a5976e10974425563a907bbc15

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -21,7 +21,7 @@ void init_cond();
  * The assembly code in start.S will finally jump here.
  */
 void init() {
-	nemu_assert(0 == 1);
+#define IA32_PAGE
 #ifdef IA32_PAGE
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
