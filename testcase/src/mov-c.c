9d66d53612c42344ed914e71899d0a6136c156a0
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 02:42:46 +0800

    >  run 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  02:42:46 up 13:09,  1 user,  load average: 0.87, 0.94, 0.96 e069e91e4ffa99954424912b879a1cfbb6a744c1

--- testcase/src/mov-c.c
+++ testcase/src/mov-c.c
@@ -1,7 +1,6 @@
 #include "trap.h"
 
 int A[2];
-int b;
 
 int main() {
 	A[0] = 0;
@@ -9,16 +8,12 @@ int main() {
 	// A[2] = 2;
 	// A[3] = 3;
 	// A[4] = 4;
-
-	b = A[0];
-	A[1] = b;
 	
 	nemu_assert(A[0] == 0);
 	nemu_assert(A[1] == 0);
 	// nemu_assert(A[2] == 2);
 	// nemu_assert(A[3] == 3);
 	// nemu_assert(A[4] == 4);
-	nemu_assert(b == 0);
 	// nemu_assert(A[5] == 3);
 
 	return 0;
