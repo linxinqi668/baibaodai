14e072429556283a3e87bba524ba074d93ac4222
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 04:03:56 +0800

    >  compile kernel 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  04:03:56 up 14:30,  1 user,  load average: 1.14, 1.04, 0.91 1787ea9d4ec302e4489017cefe99fac496fe2f44

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -77,7 +77,7 @@ uint32_t loader() {
 			// 3. 每个文件的开头都是0x0. 读取前4个字节就是魔数.
 			// 仿照另一个elf.c文件就可以写出来了
 			// nemu_assert(0 == 1);
-			ph -> p_vaddr = mm_malloc(ph -> p_vaddr,ph -> p_memsz);
+			// ph -> p_vaddr = mm_malloc(ph -> p_vaddr,ph -> p_memsz);
 			// nemu_assert(0 == 1); reached
 			uint8_t * st_addr = (uint8_t *)ph->p_vaddr;
 			// ramdisk_read((void*)(ph -> p_vaddr),ph -> p_offset,ph -> p_filesz);
