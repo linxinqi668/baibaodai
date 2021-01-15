ffce4308c3a65a02e6955704067c574b608e16e0
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-15 23:58:08 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:58:08 up 10:24,  1 user,  load average: 1.60, 1.26, 1.12 b77413376d338cca14ce7137f4dc7c0d094718e

--- nemu/src/cpu/exec/stos/stos-template.h
+++ nemu/src/cpu/exec/stos/stos-template.h
@@ -1,19 +1,28 @@
 #include "cpu/exec/template-start.h"
 
+// make_helper(concat(stos_, SUFFIX)) {
+//     // printf("safe start.\n");
+//     current_sreg = R_ES;
+//     swaddr_t addr_dest = reg_l(R_EDI);
+//     printf("here");
+
+//     MEM_W(addr_dest, REG(R_EAX));
+
+//     if (cpu.EFLAGS.DF == 0)
+//         reg_l(R_EDI) += DATA_BYTE;
+//     else
+//         reg_l(R_EDI) -= DATA_BYTE;
+//     // printf("safe end.\n\n");
+//     print_asm_template1();
+//     return 1;
+// }
+
 make_helper(concat(stos_, SUFFIX)) {
-    // printf("safe start.\n");
     current_sreg = R_ES;
-    swaddr_t addr_dest = reg_l(R_EDI);
-    printf("here");
-
-    MEM_W(addr_dest, REG(R_EAX));
-
-    if (cpu.EFLAGS.DF == 0)
-        reg_l(R_EDI) += DATA_BYTE;
-    else
-        reg_l(R_EDI) -= DATA_BYTE;
-    // printf("safe end.\n\n");
-    print_asm_template1();
+    MEM_W(reg_l(R_EDI),REG(R_EAX));
+	if (cpu.EFLAGS.DF == 0) reg_l(R_EDI) += DATA_BYTE;
+	else reg_l(R_EDI) -= DATA_BYTE;
+	print_asm("stos");
     return 1;
 }
 
