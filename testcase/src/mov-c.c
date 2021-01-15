64b1e204df1b879a501b99a4958619277f35ee27
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 02:43:27 +0800

    >  run 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  02:43:27 up 13:10,  1 user,  load average: 1.05, 0.98, 0.97 7aa24c87763b971ee0e9206182c01e0053fd43ba

--- testcase/src/mov-c.c
+++ testcase/src/mov-c.c
@@ -4,13 +4,13 @@ int A[2];
 
 int main() {
 	A[0] = 0;
-	A[1] = 1;
+	// A[1] = 1;
 	// A[2] = 2;
 	// A[3] = 3;
 	// A[4] = 4;
 	
 	nemu_assert(A[0] == 0);
-	nemu_assert(A[1] == 0);
+	// nemu_assert(A[1] == 0);
 	// nemu_assert(A[2] == 2);
 	// nemu_assert(A[3] == 3);
 	// nemu_assert(A[4] == 4);
