f0f00cfe5e5f3e39e34810b7d553b7f202631af9
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 01:47:04 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:47:04 up 12:13,  1 user,  load average: 0.93, 1.17, 1.13 4389bd77cd54cd007c4ef765abae214345e0974a

--- nemu/src/cpu/exec/jcc/jge-template.h
+++ nemu/src/cpu/exec/jcc/jge-template.h
@@ -5,12 +5,6 @@ make_helper( concat(jge_rel_, SUFFIX) ) {
     // decode
     DATA_TYPE_S rel = instr_fetch(eip + 1, DATA_BYTE);
 
-    if (eip == 0x100828)
-    {
-        printf("%d\n", rel);
-        getchar();
-    }
-
     // judge
     if ((cpu.EFLAGS.SF == cpu.EFLAGS.OF) || cpu.EFLAGS.ZF == 1)
         cpu.eip = cpu.eip + 1 + DATA_BYTE + (int32_t)rel;
