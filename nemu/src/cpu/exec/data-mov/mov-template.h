e3c014ed4c46f2ad5b6278dd07675fd08d4aaeff
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 00:17:31 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:17:31 up 10:44,  1 user,  load average: 1.05, 1.05, 1.02 9326ca9559eaa6955bdb5fbe9f43c0e9ed9ebf3e

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -13,8 +13,8 @@ make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
 make_helper(concat(mov_a2moffs_, SUFFIX)) {
-	current_sreg = R_DS;
 	swaddr_t addr = instr_fetch(eip + 1, 4);
+	current_sreg = R_DS;
 	MEM_W(addr, REG(R_EAX));
 
 	print_asm("mov" str(SUFFIX) " %%%s,0x%x", REG_NAME(R_EAX), addr);
@@ -22,8 +22,8 @@ make_helper(concat(mov_a2moffs_, SUFFIX)) {
 }
 
 make_helper(concat(mov_moffs2a_, SUFFIX)) {
-	current_sreg = R_DS;
 	swaddr_t addr = instr_fetch(eip + 1, 4);
+	current_sreg = R_DS;
 	REG(R_EAX) = MEM_R(addr);
 
 	print_asm("mov" str(SUFFIX) " 0x%x,%%%s", addr, REG_NAME(R_EAX));
