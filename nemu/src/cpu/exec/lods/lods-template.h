0610200f5d85b1d4d739bee385b347a013704de9
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 00:46:56 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:46:56 up 11:13,  1 user,  load average: 1.21, 1.12, 1.07 2e3d33b1cd644ff33622b85da6a503e2b000623f

--- nemu/src/cpu/exec/lods/lods-template.h
+++ nemu/src/cpu/exec/lods/lods-template.h
@@ -2,22 +2,10 @@
 
 make_helper(concat(lods_, SUFFIX)) {
     current_sreg = R_DS;
-    // load DATA_BYTE to EAX
-    // 地址是32位.
-    swaddr_t addr_src = reg_l(R_ESI);
-
-    DATA_TYPE val = swaddr_read(addr_src, DATA_BYTE);
-
-    // 目标不一定.
-    REG(R_EAX) = val;
-
-    if (cpu.EFLAGS.DF == 0)
-        reg_l(R_ESI) += DATA_BYTE;
-    else
-        reg_l(R_ESI) -= DATA_BYTE;
-
-    print_asm_template1();
-
+    REG(R_EAX) = MEM_R(reg_l(R_ESI));
+    if (cpu.EFLAGS.DF == 0) reg_l(R_ESI) += DATA_BYTE;
+    else reg_l(R_ESI) -= DATA_BYTE;
+	print_asm_template2();
     return 1;
 }
 
