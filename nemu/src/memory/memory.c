10beb2ad016e8140d0b2027ca5e1cf3bad87e01d
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 10:27:50 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:27:50 up 20:54,  1 user,  load average: 2.23, 1.81, 1.37 33402e30c2dfa027d461fc2d7bb418a66c9e8344

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -49,22 +49,28 @@ hwaddr_t page_translate(lnaddr_t addr){
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	// uint32_t answer = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	// return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
-	uint32_t cache_ans = L1_M_CACHE.m_cache_read(&L1_M_CACHE, addr, len);
+	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	// uint32_t cache_ans = L1_M_CACHE.m_cache_read(&L1_M_CACHE, addr, len);
 	// printf("ans: %x cache: %x\n\n\n", answer, cache_ans);
 	// assert(answer == cache_ans);
-	return cache_ans;
+	//return cache_ans;
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	// dram_write(addr, len, data);
-	L1_M_CACHE.m_cache_write(&L1_M_CACHE, addr, data, len);
+	dram_write(addr, len, data);
+	// L1_M_CACHE.m_cache_write(&L1_M_CACHE, addr, data, len);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
+	//printf("%x\n",addr);
+/*To Avoid Potential Errors (len = 1 + 3)*/
+
+// #ifdef DEBUG
+// 	assert(len == 1 || len == 2 || len == 4);
+// #endif
 	uint32_t now_offset = addr & 0xfff;
 	if (now_offset + len -1 > 0xfff){
-		Assert(0,"Cross the page boundary");
+		// Assert(0,"Cross the page boundary");
 		size_t l = 0xfff - now_offset + 1;
 		uint32_t addr_r = lnaddr_read(addr,l);
 		uint32_t addr_l = lnaddr_read(addr + l,len - l);
@@ -74,12 +80,19 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 		hwaddr_t hwaddr = page_translate(addr);
 		return hwaddr_read(hwaddr,len);
 	}
+	// return hwaddr_read(addr, len);
 }
 
 void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
+
+/*To Avoid Potential Errors (len = 1 + 3)*/
+
+// #ifdef DEBUG
+// 	assert(len == 1 || len == 2 || len == 4);
+// #endif
 	uint32_t now_offset = addr & 0xfff;
 	if (now_offset + len - 1 > 0xfff){
-		Assert(0,"Cross the page boundary");
+		// Assert(0,"Cross the page boundary");
 		size_t l = 0xfff - now_offset + 1;
 		lnaddr_write(addr,l,data & ((1 << (l << 3)) - 1));
 		lnaddr_write(addr + l,len - l,data >> (l << 3));
@@ -87,6 +100,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 		hwaddr_t hwaddr = page_translate(addr);
 		hwaddr_write(hwaddr, len, data);
 	}
+	// hwaddr_write(addr, len, data);
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
