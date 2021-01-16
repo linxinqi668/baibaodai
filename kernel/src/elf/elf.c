5a9ce1bdc9c05b590e652c0b96036fc764eaa34b
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 09:08:26 +0800

    >  compile kernel 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:08:26 up 19:35,  1 user,  load average: 0.87, 0.69, 0.83 5ef46c8767ed03c51fd497e73d8ee7079662b9dd

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -54,8 +54,6 @@ uint32_t loader() {
 			 */
 			memset((void*)(ph -> p_vaddr + ph -> p_filesz),0,ph -> p_memsz - ph -> p_filesz);
 
-			ph ++;
-
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
@@ -63,6 +61,7 @@ uint32_t loader() {
 			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
 #endif
 		}
+		ph ++;
 	}
 	nemu_assert(0 == 1);
 	volatile uint32_t entry = elf->e_entry;
