11789e2ed85d89239caf8bfc316acc76aa9cfde1
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 10:36:34 +0800

    >  compile NEMU 3019244317 nobug Linux nobug-KLV-WX9 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:36:34 up 21:03,  1 user,  load average: 2.37, 1.82, 1.54 583ad48b94bc949534fe093b31961a6433b629ed

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,6 +1,5 @@
 #include "common.h"
 #include "memory.h"
-#include <stdio.h>
 
 void init_page();
 void init_serial();
@@ -16,37 +15,30 @@ void video_mapping_read_test();
 void video_mapping_clear();
 
 void init_cond();
-
+//int x=0;
 /* Initialization phase 1
  * The assembly code in start.S will finally jump here.
  */
 void init() {
 #ifdef IA32_PAGE
+	
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
 	 * Before setting up correct paging, no global variable can be used. */
 	init_page();
-	// nemu_assert(0 == 1);
+	//x=1;
 	/* After paging is enabled, transform %esp to virtual address. */
 	asm volatile("addl %0, %%esp" : : "i"(KOFFSET));
 #endif
-	// nemu_assert(0 == 1);
-	/* Jump to init_cond() to continue initialization. */
-	// int x = 0;
-	// nemu_assert(x == 1); // reached this line.
-	
-	asm volatile("jmp *%0" : : "r"(init_cond)); // 直接跳到0x800e70了?
-	// 查看反汇编可以知道0x800e70没错, 但是kernel不应该待在这里, 说明kernel存放位置不对.
-	// 它应该待在0x100000左右.
-	// nemu_assert(x == 1); // not reach here.
 
+	/* Jump to init_cond() to continue initialization. */
+	asm volatile("jmp *%0" : : "r"(init_cond));
+	// init_cond();
 	panic("should not reach here");
 }
 
 /* Initialization phase 2 */
 void init_cond() {
-	// int check = 0;
-	// nemu_assert(check == 1);
 #ifdef IA32_INTR
 	/* Reset the GDT, since the old GDT in start.S cannot be used in the future. */
 	init_segment();
@@ -74,6 +66,7 @@ void init_cond() {
 #ifdef IA32_PAGE
 	/* Initialize the memory manager. */
 	init_mm();
+	
 #endif
 
 	/* Output a welcome message.
@@ -86,12 +79,12 @@ void init_cond() {
 	/* Write some test data to the video memory. */
 	video_mapping_write_test();
 #endif
-
+	video_mapping_write_test();
 	/* Load the program. */
-	// nemu_assert(0 == 1); reached here.
 	uint32_t eip = loader();
-	nemu_assert(0 == 1);
 	
+	// video_mapping_read_test();
+	// video_mapping_clear();
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
 	 * the test data is written sucessfully.
@@ -101,13 +94,13 @@ void init_cond() {
 	/* Clear the test data we just written in the video memory. */
 	video_mapping_clear();
 #endif
-	// nemu_assert(0 == 1);
+
 #ifdef IA32_PAGE
 	/* Set the %esp for user program, which is one of the
 	 * convention of the "advanced" runtime environment. */
 	asm volatile("movl %0, %%esp" : : "i"(KOFFSET));
 #endif
-	// assert(0 == 1); // not reached this line.
+
 	/* Keep the `bt' command happy. */
 	asm volatile("movl $0, %ebp");
 	asm volatile("subl $16, %esp");
